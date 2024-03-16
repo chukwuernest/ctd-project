109afd146480ff18bf3cde14dd3cfe8ca700930b
Leochukwu <chukwu17mc@gmail.com>
2024-03-16 06:20:46 -1100

    error update

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 This is my github repo (https://github.com/chukwuernest)
 
 For the react icon install react icon using(npm install react-icons --save)
+
 Also i install axios to use in getting the GET request from the Api( npm install react-axios)
+
 i got the api from (https://open-meteo.com/)
+
+the api from the meteo app that i use,( api =
+https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,rain,snowfall,cloud_cover,wind_speed_10m&models=icon_global,gem_global,gem_regional)
+
+I use Math.round to change the speed and temperature to whole number
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,32 +1,54 @@
 import React, { useState, useEffect } from 'react'
 import { CiSearch } from 'react-icons/ci'
-import img from './Data/img2.jpg'
 import axios from 'axios'
 
 function HomePage() {
-  const [weather, setWeather] = useState({
-    celcius: 19,
-    humidity: 20,
-    speed: 20,
-    latitude: '54.32',
+  const [data, setData] = useState({
+    celcius: 9,
+    humidity: 4,
+    speed: 2,
+    latitude: 50.32,
+    longitude: 2.22222,
+    Elevation: 10,
+    cloud: 10,
+    rain: 10,
+    snowfall: 10,
   })
-  useEffect(() => {
-    const url =
-      'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relative_humidity_2m,wind_speed_10m'
+  const [latitude, setlatitude] = useState('')
+  const [error, setError] = useState('')
+  const handleClick = () => {
+    if (Math.round(latitude) !== '') {
+      const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=13.41&current=temperature_2m,relative_humidity_2m,rain,snowfall,cloud_cover,wind_speed_10m&models=icon_global,gem_global,gem_regional`
 
-    axios
-      .get(url)
-      .then((res) => {
-        setWeather({
-          ...weather,
-          celcius: res.weather.current.temperature_2m,
-          latitude: res.weather.latitude,
-          humidity: res.weather.curent.realative_humidity_2m,
-          speed: res.weather.current.wind_speed_10m,
+      axios
+        .get(url)
+        .then((res) => {
+          // to get the object to do what models that you choose to display use
+          console.log(res.data)
+          setData({
+            ...data,
+            celcius: res.data.current.temperature_2m,
+            humidity: res.data.current.relative_humidity_2m,
+            speed: res.data.current.wind_speed_10m,
+            latitude: res.data.latitude,
+            longitude: res.data.longitude,
+            elevation: res.data.elevation,
+            cloud: res.data.current.cloud_cover,
+            rain: res.data.current.rain,
+            snowfall: res.data.current.snowfall,
+          })
+          setError('')
         })
-      })
-      .catch((err) => console.log(err))
-  }, [])
+        .catch((err) => {
+          if (err.response.status == 400) {
+            setError('invalid Latitude must be in range of -90° to 90°')
+          } else {
+            setError('')
+          }
+          console.log(err)
+        })
+    }
+  }
   return (
     <div className='section'>
       <div className='section-container'>
@@ -34,30 +56,40 @@ function HomePage() {
           <div className='search'>
             <input
               type='text'
-              placeholder='enter city latitude'
+              placeholder='enter city latitude E.g 50 or 50.56552'
               className='search-input'
+              onChange={(e) => setlatitude(e.target.value)}
             />
             <button className='search-btn'>
-              <CiSearch className='search-icon' />
+              <CiSearch className='search-icon' onClick={handleClick} />
             </button>
           </div>
+          <div className='error'>
+            <h4>{error}</h4>
+          </div>
           <div className='elements'>
-            <img src={img} alt='weather image' />
-            <h2>{weather.celcius}°C</h2>
-            <h3>temperature</h3>
+            <img src='/data/tem.png' alt='weather image' />
+            <h2>{Math.round(data.celcius)}°C</h2>
+            <h3>
+              latitude {data.latitude}°N,longitude{data.longitude}°E
+            </h3>
+            <h4>{data.elevation}m above sea level</h4>
+            <h4>
+              rainfall:{data.rain},cloud:{data.cloud},snowfall:{data.snowfall}
+            </h4>
             {/* this is the section that deals with humidity and wind */}
             <div className='info'>
               <div className='info-more'>
-                <img src={img} alt='humidity' />
+                <img src='/data/humidity.png' alt='humidity' />
                 <div className='humidity'>
-                  <p>20%</p>
+                  <p>{data.humidity}%</p>
                   <p>humidity</p>
                 </div>
               </div>
               <div className='info-more'>
-                <img src={img} alt='win' />
+                <img src='/data/wind.png' alt='win' />
                 <div className='win'>
-                  <p>20 km/h</p>
+                  <p>{Math.round(data.speed)} km/h</p>
                   <p>wind</p>
                 </div>
               </div>
